847cb3d2567e3335b411c9fc3107957c90ced740
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-17 21:33:37 +0100

    remove base variable from client2

--- src/client2.js
+++ src/client2.js
@@ -4,8 +4,7 @@ const { taxThreshold } = require('./tax-utils');
 
 const rawReading = acquireReading();
 const aReading = new Reading(rawReading);
-const base = aReading.baseCharge;
-const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
+const taxableCharge = Math.max(0, aReading.baseCharge - taxThreshold(aReading.year));
 
-console.log(`base charge is ${base}`);
+console.log(`base charge is ${aReading.baseCharge}`);
 console.log(`taxable charge is ${taxableCharge}`);
