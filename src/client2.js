889e45f9351854cb56f0fc779d7830c2ad7282db
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-17 21:46:15 +0100

    update client2 to use taxableCharge getter instead of TaxableChargeFn

--- src/client2.js
+++ src/client2.js
@@ -1,10 +1,9 @@
 const { acquireReading } = require('./acquire-reading');
 const { Reading } = require('./reading');
-const { taxableChargeFn } = require('./tax-utils');
 
 const rawReading = acquireReading();
 const aReading = new Reading(rawReading);
-const taxableCharge = taxableChargeFn(aReading);
+const taxableCharge = aReading.taxableCharge;
 
 console.log(`base charge is ${aReading.baseCharge}`);
 console.log(`taxable charge is ${taxableCharge}`);
