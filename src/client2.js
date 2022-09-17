5faf3182f0181e9afe4a617cfdf44d072ef376e1
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-17 21:40:39 +0100

    update client2 to start using taxableChargeFn

--- src/client2.js
+++ src/client2.js
@@ -1,10 +1,10 @@
 const { acquireReading } = require('./acquire-reading');
 const { Reading } = require('./reading');
-const { taxThreshold } = require('./tax-utils');
+const { taxableChargeFn } = require('./tax-utils');
 
 const rawReading = acquireReading();
 const aReading = new Reading(rawReading);
-const taxableCharge = Math.max(0, aReading.baseCharge - taxThreshold(aReading.year));
+const taxableCharge = taxableChargeFn(aReading);
 
 console.log(`base charge is ${aReading.baseCharge}`);
 console.log(`taxable charge is ${taxableCharge}`);
