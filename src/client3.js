cb609dde9e1064ad0b1dcb04cab79a7fde71916a
Kaio Silveira <silveira.kaio@icloud.com>
2022-09-17 21:28:47 +0100

    change client3 to use calculateBaseCharge from the Reading class instance

--- src/client3.js
+++ src/client3.js
@@ -1,13 +1,8 @@
 const { Reading } = require('./reading');
 const { acquireReading } = require('./acquire-reading');
-const { baseRate } = require('./tax-utils');
 
 const rawReading = acquireReading();
 const aReading = new Reading(rawReading);
-const basicChargeAmount = calculateBaseCharge(aReading);
+const basicChargeAmount = aReading.calculateBaseCharge();
 
 console.log(`basic charge amount is ${basicChargeAmount}`);
-
-function calculateBaseCharge(aReading) {
-  return baseRate(aReading.month, aReading.year) * aReading.quantity;
-}
